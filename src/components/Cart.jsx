7b499eb2687dc4d67b26a192e9bcc32d7b772dc8
anthonywandera <wanderatony92@gmail.com>
2024-12-15 11:33:43 +0300

    Opening the cart in the modal via a new component

---
+++ src/components/Cart.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal.jsx";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../util/formatting.js";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UseProgressContext.jsx";
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce((acc, item) => {
+    return acc + item.price * item.quantity;
+  }, 0);
+
+  return (
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => {
+          return (
+            <li key={item.id}>
+              {item.name} - {item.quantity}
+            </li>
+          );
+        })}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly>Close</Button>
+        <Button>Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
